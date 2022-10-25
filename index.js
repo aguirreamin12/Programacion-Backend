e92385715e86997618fcf05f766d112f95bca31d
Santino Aguirre Amin <santinoaguirre2004@gmail.com>
2022-10-25 19:36:56 -0300

    Desafio Clase 2 corregido

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ class Usuario {
     }
 
     getFullName(){
-        console.log(`${this.nombre} ${this.apellido}`)
+        return `${this.nombre} ${this.apellido}`
     }
 
     addMascota(mascota){
@@ -17,25 +17,27 @@ class Usuario {
     countMascotas(){
         const cantidad = this.mascotas.length
         const texto = cantidad > 1 ? `Tiene ${cantidad} mascotas` : 'Tiene una sola mascota';
-        console.log(`${this.nombre} ${cantidad === 0 ? 'No tiene mascotas': texto}`)
+        return `${this.nombre} ${cantidad === 0 ? 'No tiene mascotas': texto}`
     }
 
     addBook(nombre, autor){
-        this.libros.push({nombre, autor})
+        this.libros.push({nombre: nombre, autor: autor})
     }
 
     getBookNames(){
-        console.log(this.libros.map((e) => e.libros))
+        return this.libros.map((e) => e.libros)
     }
 }
 
 const usuario = new Usuario('Santino', 'Aguirre')
-Usuario.getFullName()
+usuario.getFullName()
 
-Usuario.addMascota('Perro')
-Usuario.addMascota('Gato')
-Usuario.countMascotas()
+usuario.addMascota('Perro')
+usuario.addMascota('Gato')
+usuario.countMascotas()
 
-Usuario.addBook('Bajo la misma estrella', 'John Green')
-Usuario.addBook('El lobo de Wall Street', 'Jordan Belfort')
-Usuario.getBookNames()
+usuario.addBook('Bajo la misma estrella', 'John Green')
+usuario.addBook('El lobo de Wall Street', 'Jordan Belfort')
+usuario.getBookNames()
+
+console.log(usuario)
